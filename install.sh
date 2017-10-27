0969560f85a68471dabb50ffe3663e5ea2479ae8
Evgeny Shurupov <bolteg86@ya.ru>
2017-10-27 15:35:05 +0400

    little fixes

--- install.sh
+++ install.sh
@@ -3,12 +3,6 @@
 export USER_NAME=username
 export USER_PASSWORD=password
 
-if [ -d /opt/scripts ]; then
-    cd /opt/scripts
-    chmod +x ./install.sh
-    ./install.sh
-fi
-
 if [ -d ../scripts ]; then
     cd ../scripts
     chmod +x ./install.sh
